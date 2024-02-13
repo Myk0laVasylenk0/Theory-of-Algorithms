078b3c598141d24ece0252928b2c11956db16ceb
Myk0laVasylenk0 <vasylenkomykolawork@gmail.com>
2024-02-13 14:33:07 +0200

    first one

---
+++ Computer practicums 1/proga/proga/Program.cs
@@ -0,0 +1,42 @@
+﻿
+int num_elements, index_min, min_element, i;
+
+int[] numbers = { 2, 4, 5, 7, 0, 4, 6, 6, 1, 4, -1 };
+
+
+void FindMinElement()
+{
+    min_element = numbers[0];
+    num_elements = numbers.GetLength(0);
+    i = 0;
+    index_min = 0;
+    min_element = numbers[0];
+
+
+    while (i < num_elements)
+    {
+        if (min_element > numbers[i])
+        {
+            min_element = numbers[i];
+            index_min = i;
+            i++;
+        }
+        else 
+        {
+            i++;
+        }
+    }
+    string message = $"minimal element in array: {min_element} with index: {index_min}";
+
+    Console.WriteLine(message);
+    
+}
+
+
+FindMinElement();  
+Console.ReadKey();
+
+
+
+
+
